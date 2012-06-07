851f97583851bcc102e339f9ca5e7c1de9e91828
Nathan M <nathanm@gmail.com>
2012-06-06 17:36:22 -0700

    converted tabs to spaces

--- element_positioner.js
+++ element_positioner.js
@@ -1,53 +1,53 @@
 var ElementPositioner = function(options){
-	this.options = $.extend({
-		elements : null,
-		cssPrefix : ''
-	},options);
-	
-	this.draggable = null;
-	this.active = false;
-	this.bindActivationKeypress();
-		
+  this.options = $.extend({
+    elements : null,
+    cssPrefix : ''
+  },options);
+  
+  this.draggable = null;
+  this.active = false;
+  this.bindActivationKeypress();
+    
 };
 
 ElementPositioner.prototype.bindActivationKeypress = function(){
   $('body').keypress($.proxy(function(e){
     if(e.charCode == 101){
-			if(this.active){
-				this.stop();
-			}else{
-				this.start();
-			}
-		}
+      if(this.active){
+        this.stop();
+      }else{
+        this.start();
+      }
+    }
   },this));
 };
 
 ElementPositioner.prototype.start = function(){
-	this.removeClickBindingsFromElements();
-	this.draggable = this.options.elements.draggable({stack : '*'});
-	this.active = true;
+  this.removeClickBindingsFromElements();
+  this.draggable = this.options.elements.draggable({stack : '*'});
+  this.active = true;
 };
 
 ElementPositioner.prototype.stop = function(){
-	this.restoreClickBindingsToElements();
-	this.draggable.draggable("option","disabled",true);
-	this.active = false;
-	this.dumpCSS();
+  this.restoreClickBindingsToElements();
+  this.draggable.draggable("option","disabled",true);
+  this.active = false;
+  this.dumpCSS();
 };
 
 ElementPositioner.prototype.dumpCSS = function(){
-	var styleString = "";
- 	this.options.elements.each($.proxy(function(i,e){
- 			e = $(e);
- 	    styleString += this.options.cssPrefix + '#' + e.attr('id') + '{' + 'left:' + e.css('left') + ';' + 'top:' + e.css('top') + ';' + 'z-index:' + e.css('z-index') + ';' + '}'+"\n";
- 	  },this));
- 	  console.log(styleString);
+  var styleString = "";
+  this.options.elements.each($.proxy(function(i,e){
+      e = $(e);
+      styleString += this.options.cssPrefix + '#' + e.attr('id') + '{' + 'left:' + e.css('left') + ';' + 'top:' + e.css('top') + ';' + 'z-index:' + e.css('z-index') + ';' + '}'+"\n";
+    },this));
+    console.log(styleString);
 };
 
 ElementPositioner.prototype.removeClickBindingsFromElements = function(){
-	this.options.elements.unbind('click');
+  this.options.elements.unbind('click');
 };
 
 ElementPositioner.prototype.restoreClickBindingsToElements = function(){
-	// todo
+  // todo
 };
